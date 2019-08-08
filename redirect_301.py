98fa75b1880427d88ad3099a3f3be4369f290214
ItsDg4u <48048739+ItsDg4u@users.noreply.github.com>
2019-08-09 00:07:42 +0530

    Updated redirect_301.py

--- redirect_301.py
+++ redirect_301.py
@@ -1,12 +1,12 @@
 import http.server
 import socketserver
 
-class FakeRedirect(http.server.SimpleHTTPRequestHandler):
+class Redirect(http.server.SimpleHTTPRequestHandler):
    def do_GET(self):
        print (self.path)
        self.send_response(301)
-       new_path = '%s%s'%('http://127.0.0.1:9091', self.path)
+       new_path = '%s%s'%('http://localhost:9091', self.path)
        self.send_header('Location', new_path)
        self.end_headers()
 
-socketserver.TCPServer(("", 9090), FakeRedirect).serve_forever()
+socketserver.TCPServer(("", 9090), Redirect).serve_forever()
